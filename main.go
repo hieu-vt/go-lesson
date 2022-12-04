dd226535b740e7c361882c0c83d7848c8d47cd0d
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2022-12-04 21:58:20 +0700

    Learn basic gorm and mysql

--- main.go
+++ main.go
@@ -15,6 +15,14 @@ type Note struct {
 	Content string `json:"content" gorm:"column:content;"`
 }
 
+type NoteUpdate struct {
+	Title *string `json:"title" gorm:"column:title;"`
+}
+
+func (Note) TableName() string {
+	return "notes"
+}
+
 func main() {
 	dsn := os.Getenv("DBConnectionStr")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -23,9 +31,42 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	newNote := Note{Title: "This is domo note", Content: "This is content of demo note"}
+	// inset data to database
+	//newNote := Note{Title: "This is domo note", Content: "This is content of demo note"}
+	//
+	//if err := db.Create(&newNote); err != nil {
+	//	log.Println(err)
+	//}
+	//
+	//fmt.Println(newNote)
 
-	db.Create(&newNote)
+	// get data
+	var notes []Note
 
-	fmt.Println(newNote)
+	db.Where("status = ?", 1).Find(&notes)
+
+	fmt.Println("Notes: ", notes)
+
+	var note Note
+
+	result := db.Where("id = ?", 3).First(&note)
+
+	if err := result.Error; err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Result: ", note)
+
+	// delete item
+	//db.Table(Note{}.TableName()).Where("id = ?", 1).Delete(nil)
+
+	// update
+	//db.Table(Note{}.TableName()).Where("id = 2").Updates(map[string]interface{}{
+	//	"title": "Demo 1",
+	//})
+	//note.Title = "Demo 2"
+	//db.Table(Note{}.TableName()).Where("id = 3").Updates(&note)
+
+	newTitle := "Demo 4   "
+	db.Table(Note{}.TableName()).Where("id = 3").Updates(&NoteUpdate{Title: &newTitle})
 }
