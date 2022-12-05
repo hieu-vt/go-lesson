c1647d8786b1654c43c17cf16cba39eb7cea72e6
hieu.vu <hieu.vu@admins-iMac.local>
2022-12-05 14:53:57 +0700

    learn association gorm

--- lession_5_leaning.md
+++ lession_5_leaning.md
@@ -9,4 +9,8 @@
 * Liên quan đến khóa ngoại (foreign key)
 	* Tại sao không cần thiết dùng trong mọi trường hợp
 	* Học lại về primary key, foreign key, index, quan hệ giữa Các bảng trong db
+* Start leaning GORM with Associations
+  * **Belongs to:** là thiết lập one-to-one connection với một **model** khác
+    * Ví dụ: 2 model user vs profile, Một user chỉ có 1 profile và 1 profile chỉ dành cho 1 user
+  * has one: 
 
--- main.go
+++ main.go
@@ -23,13 +23,47 @@ func (Note) TableName() string {
 	return "notes"
 }
 
+type User struct {
+	Id   int    `json:"id,omitempty" gorm:"column:id;"`
+	Name string `json:"name" gorm:"column:name;"`
+}
+
+// `Profile` belongs to `User`, `UserID` is the foreign key
+
+type Profile struct {
+	Id   int    `json:"id,omitempty" gorm:"column:id"`
+	User User   `gorm:"association_foreignKey:name"`
+	Name string `json:"name" gorm:"column:name;"`
+}
+
 func main() {
+	os.Setenv("DBConnectionStr", "root:ead8686ba57479778a76e@tcp(127.0.0.1:3306)/food_delivery?charset=utf8mb4&parseTime=True&loc=Local")
 	dsn := os.Getenv("DBConnectionStr")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	// insert profile of user
+	newProfile := Profile{Name: "hieu thu hai", User: User{Name: "Hieu Vu"}}
+
+	if errCrProfile := db.Create(&newProfile); errCrProfile != nil {
+		log.Fatalln(newProfile)
+	}
+
+	fmt.Println(newProfile)
+
+	// insert data user to database
+	//newUser := User{
+	//	Name: "Vu Thanh Hieu",
+	//}
+	//
+	//if errCrUser := db.Create(&newUser); errCrUser != nil {
+	//	log.Fatalln(errCrUser)
+	//}
+	//
+	//fmt.Println(newUser)
 
 	// inset data to database
 	//newNote := Note{Title: "This is domo note", Content: "This is content of demo note"}
@@ -41,21 +75,21 @@ func main() {
 	//fmt.Println(newNote)
 
 	// get data
-	var notes []Note
-
-	db.Where("status = ?", 1).Find(&notes)
-
-	fmt.Println("Notes: ", notes)
-
-	var note Note
-
-	result := db.Where("id = ?", 3).First(&note)
-
-	if err := result.Error; err != nil {
-		log.Println(err)
-	}
-
-	fmt.Println("Result: ", note)
+	//var notes []Note
+	//
+	//db.Where("status = ?", 1).Find(&notes)
+	//
+	//fmt.Println("Notes: ", notes)
+	//
+	//var note Note
+	//
+	//result := db.Where("id = ?", 3).First(&note)
+	//
+	//if err := result.Error; err != nil {
+	//	log.Println(err)
+	//}
+	//
+	//fmt.Println("Result: ", note)
 
 	// delete item
 	//db.Table(Note{}.TableName()).Where("id = ?", 1).Delete(nil)
@@ -66,7 +100,7 @@ func main() {
 	//})
 	//note.Title = "Demo 2"
 	//db.Table(Note{}.TableName()).Where("id = 3").Updates(&note)
-
-	newTitle := "Demo 4   "
-	db.Table(Note{}.TableName()).Where("id = 3").Updates(&NoteUpdate{Title: &newTitle})
+	//
+	//newTitle := "Demo 4   "
+	//db.Table(Note{}.TableName()).Where("id = 3").Updates(&NoteUpdate{Title: &newTitle})
 }
